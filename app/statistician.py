65e257c337c06ef860091e9eadc7934b7854f98b
normaloide <edoardoalejandroferrari@gmail.com>
2020-11-19 19:56:42 +0100

    :heavy_check_mark: Issue solved

--- app/statistician.py
+++ app/statistician.py
@@ -39,21 +39,41 @@ class Stats:
         # take data only from the selected region
         df = df.set_index("denominazione_regione").filter(like=self.region, axis=0)
 
-        # set start and end data retrival date
+        # set start and end data retrival date, create a new index and drop 'data'
         start = df.iloc[0]["data"]
         end = df.iloc[-1]["data"]
         df.set_index(pd.date_range(start=start, end=end, freq="D"), inplace=True)
+        df.drop("data", axis=1, inplace=True)
 
-        # plot everything
-        ax = df.plot(grid=True, kind="area", title=self.region, color="royalblue")
+        # separate positive values from negative values
+        pos = df.clip(lower=0)
+        neg = df.clip(upper=0)
 
-        ax.legend([self.query], loc="upper center")
+        # plot everything, except in case there are no negative values
+        fig, ax = plt.subplots()
+        try:
+            pos.plot.area(ax=ax, stacked=False, linewidth=1)
+            max = pos.sum(axis=1).max()
+
+            neg.plot.area(ax=ax, stacked=False, linewidth=1)
+            min = neg.sum(axis=1).min()
+        except:
+            min = 0
+            pass
+
+        # set x axis
         ax.set_xlabel("week")
-        ax.set_ylabel("value")
-
+        plt.axhline(0, color="k")
         ax.xaxis.set_major_locator(dates.WeekdayLocator(byweekday=MO, interval=1))
         ax.xaxis.set_major_formatter(dates.DateFormatter("%m-%d"))
 
+        # set y axis
+        ax.set_ylabel("value")
+
+        # plot other details
+        ax.set_ylim([min, max])
+        ax.grid(color="k", linestyle="-", linewidth=0.1)
+        ax.legend([self.query], loc="upper center")
         plt.gcf().autofmt_xdate()
         plt.get_current_fig_manager().resize(1300, 600)
         plt.show()
